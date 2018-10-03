6853403d52ffaeb34b34a60e8bdfbdc03a063e96
Sagar Deshmukh <sagardeshmukh336@gmail.com>
2018-10-03 08:57:51 +0530

    updated code

--- app/src/main/java/com/example/padcc/visitorsapplication/MainActivity.java
+++ app/src/main/java/com/example/padcc/visitorsapplication/MainActivity.java
@@ -1,13 +1,105 @@
 package com.example.padcc.visitorsapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+    DatabaseHandler db;
+    ListView listView;
+    Button btnview;
+    TextView fn,ln,ph,em,tech,gen;
+    List<Visitor> visitorList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    //    fn=(TextView)findViewById(R.id.textfname);
+     //   ln=(TextView)findViewById(R.id.textlname);
+       // ph=(TextView)findViewById(R.id.textphone);
+     //   em=(TextView)findViewById(R.id.textemail);
+     // tech=(TextView)findViewById(R.id.texttechnique);
+       // gen=(TextView)findViewById(R.id.textgender);
+
+        btnview=(Button) findViewById(R.id.button_view);
+
+        listView = (ListView) findViewById(R.id.listview1);
+        db=new DatabaseHandler(MainActivity.this);
+
+        visitorList=db.getAllVisitors();
+        CustomAdapter customAdapter=new CustomAdapter();
+        listView.setAdapter(customAdapter);
+
+
+
+
+    }
+
+
+    class CustomAdapter extends BaseAdapter {
+        Visitor visitor=new Visitor();
+        @Override
+        public int getCount() {
+            return visitorList.size();
+        }
+
+        @Override
+        public Object getItem(int i) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int i) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup viewGroup) {
+
+            view = getLayoutInflater().inflate(R.layout.visitorslist, null);
+
+            final TextView textViewfname1 = (TextView) view.findViewById(R.id.textfname);
+            final TextView textViewtechnique1 = (TextView) view.findViewById(R.id.texttechnique);
+
+
+            Button btnview=(Button)view.findViewById( R.id.button_view) ;
+
+            textViewfname1.setText(visitorList.get(position).getVfirstnName());
+            textViewtechnique1.setText(visitorList.get(position).getVTechnique());
+
+            visitor.setVfirstnName(visitorList.get(position).getVfirstnName());
+            visitor.setVLastName(visitorList.get(position).getVLastName());
+            visitor.setVPhone(visitorList.get(position).getVPhone());
+            visitor.setVEmail(visitorList.get(position).getVEmail());
+            visitor.setVTechnique(visitorList.get(position).getVTechnique());
+            visitor.setVgender(visitorList.get(position).getVgender());
+            btnview.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent intent=new Intent(MainActivity.this,Details.class);
+                intent.putExtra("firstname",visitor.getVfirstnName());
+                intent.putExtra("lastname",visitor.getVLastName());
+                intent.putExtra("phonenumber",visitor.getVPhone());
+                intent.putExtra("emailaddress",visitor.getVEmail());
+                intent.putExtra("techni",visitor.getVTechnique());
+                intent.putExtra("gend",visitor.getVgender());
+
+                    startActivity(intent);
+                }
+            });
+
+            return view;
+        }
     }
 }
